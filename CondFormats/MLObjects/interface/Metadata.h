4f13b53bcf039dd778cf7aad5f3a83b711d30884
hyejin kwon <hj.kwon@cern.ch>
2025-07-07 18:39:35 +0200

    ML Objects init
    add collection, esproducer
    
    code cleaning
    
    decouple metadata vector
    
    (cherry picked from commit e993a2c4ab96a09df8748cce462f00efbc7d97f1)

---
+++ CondFormats/MLObjects/interface/Metadata.h
@@ -0,0 +1,29 @@
+#ifndef CondFormats_MLObjects_Metadata_h
+#define CondFormats_MLObjects_Metadata_h
+
+#include <string>
+#include "CondFormats/Serialization/interface/Serializable.h"
+
+class Metadata {
+public:
+    Metadata() {}
+    Metadata(std::string model_name, int version, std::string hash) : model_name_(model_name), version_(version), hash_(hash) {}
+
+    std::string model_name() const { return model_name_; }
+    int version() const { return version_; }
+    std::string hash() const { return hash_; }
+
+    void set_model_name(const std::string& name) { model_name_ = name; }
+    void set_version(int version) { version_ = version; }
+    void set_hash(const std::string& hash) { hash_ = hash; }
+
+private:
+
+    std::string model_name_;
+    int version_;
+    std::string hash_;
+
+    COND_SERIALIZABLE;
+};
+
+#endif
---
+++ CondFormats/MLObjects/test/testSerializationMetadata.cpp
@@ -0,0 +1,6 @@
+#include "CondFormats/Serialization/interface/Test.h"
+#include "CondFormats/MLObjects/interface/Metadata.h"
+
+int main() { 
+	testSerialization<Metadata>(); 
+}
